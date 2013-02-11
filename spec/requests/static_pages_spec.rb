25f7d5a9bbe8bfa8e785d746ea512be3612cead4
Anthony Weiss <anthony@smartebook.com>
2013-02-11 17:46:37 +0900

    Fix readme. Update spec syntax to rb 1.9

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,48 +7,48 @@ describe "Static Pages" do
   describe "Home Page" do
 	it "should have the base title" do
 	  visit '/static_pages/home'
-	  page.should have_selector('title', :text => "#{base_title}")
+	  page.should have_selector('title', text: "#{base_title}")
 	end
 	it "should not have a custom title" do
 	  visit '/static_pages/home'
-	  page.should_not have_selector('title', :text => " | Home")
+	  page.should_not have_selector('title', text: " | Home")
 	end
 	it "should have the h1 'Sample App'" do
 	  visit '/static_pages/home'
-	  page.should have_selector('h1', :text => 'Sample App')
+	  page.should have_selector('h1', text: 'Sample App')
     end
   end
 
   describe "Help Page" do
 	it "should have the right title" do
 	  visit '/static_pages/help'
-	  page.should have_selector('title', :text => "#{base_title} | Help")
+	  page.should have_selector('title', text: "#{base_title} | Help")
 	end
 	it "should have the h1 'Help'" do
 	  visit '/static_pages/help'
-	  page.should have_selector('h1', :text => 'Help')
+	  page.should have_selector('h1', text: 'Help')
 	end
   end
 
   describe "About Page" do
 	it "should have the right title" do
 	  visit '/static_pages/about'
-	  page.should have_selector('title', :text => "#{base_title} | About Us")
+	  page.should have_selector('title', text: "#{base_title} | About Us")
 	end
 	it "should have the h1 'About Us'" do
 	  visit '/static_pages/about'
-	  page.should have_selector('h1', :text => 'About Us')
+	  page.should have_selector('h1', text: 'About Us')
 	end
   end
 
   describe "Contact Page" do
 	it "should have the right title" do
 	  visit '/static_pages/contact'
-	  page.should have_selector('title', :text => "#{base_title} | Contact Us")
+	  page.should have_selector('title', text: "#{base_title} | Contact Us")
 	end
 	it "should have the h1 'Contact Us'" do
 	  visit '/static_pages/contact'
-	  page.should have_selector('h1', :text => 'Contact Us')
+	  page.should have_selector('h1', text: 'Contact Us')
 	end
   end
 
