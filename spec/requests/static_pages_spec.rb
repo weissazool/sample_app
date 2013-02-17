437df6b1c632419227d5ac2aea8a4857f7875be9
Anthony Weiss <anthony@weissazool.com>
2013-02-17 13:00:53 +0900

    Add tests that clicking the links go to the right place.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,6 +42,24 @@ describe "Static Pages" do
 	it_should_behave_like "all static pages"
   end
 
+  it "should have the right links on the layout" do
+	visit root_path
+	click_link "About"
+	page.should have_selector 'title', text: full_title("About Us")
+	click_link "Help"
+	page.should have_selector 'title', text: full_title("Help")
+	click_link "Contact"
+	page.should have_selector 'title', text: full_title("Contact Us")
+	click_link "Home"
+	page.should have_selector 'title', text: full_title('')
+	page.should_not have_selector 'title', text: "Home"
+	click_link "Sign up now!"
+	page.should have_selector 'title', text: full_title("Sign up")
+	click_link "sample app"
+	page.should have_selector 'title', text: full_title('')
+	page.should_not have_selector 'title', text: "Home"
+  end
+
 end
 
 
