8f464f24d2f2f5980be537d92570fd4f7db84379
Anthony Weiss <anthony@weissazool.com>
2013-02-17 12:50:05 +0900

    Refactor tests.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,42 @@ describe "Static Pages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+	it { should have_selector('title', text: full_title(page_title)) }
+	it { should have_selector('h1', text: heading) }
+  end
+
   describe "Home Page" do
 	before { visit root_path }
+	let(:page_title) { '' }
+	let(:heading)	{ 'Sample App' }
 	
-	it { should have_selector('title', text: full_title('')) }
-	it { should have_selector('h1', text: 'Sample App') }
+	it_should_behave_like "all static pages"
+	it { should_not have_selector('title', text: '| Home') }
   end
 
   describe "Help Page" do
 	before { visit help_path }
-
-	it { should have_selector('title', text: full_title('Help')) }
-	it { should have_selector('h1', text: 'Help') }
+	let(:page_title) { 'Help' }
+	let(:heading)	{ 'Help' }
+	
+	it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
 	before { visit about_path }
-
-	it { should have_selector('title', text: full_title('About Us')) }
-	it { should have_selector('h1', text: 'About Us') }
+	let(:page_title) { 'About Us' }
+	let(:heading)	{ 'About Us' }
+	
+	it_should_behave_like "all static pages"
   end
 
   describe "Contact Page" do
 	before { visit contact_path }
-
-	it { should have_selector('title', text: full_title('Contact Us')) }
-	it { should have_selector('h1', text: 'Contact Us') }
+	let(:page_title) { 'Contact Us' }
+	let(:heading)	{ 'Contact Us' }
+	
+	it_should_behave_like "all static pages"
   end
 
 end
